71f21dd0d88d7a3aaf2c1d64d32c666ac589e43b
Yecheng Shao <shaoyecheng@zju.edu.cn>
2020-03-22 01:27:13 +0800

    Add noise to motor

--- project/proj3_fly/src/Env.py
+++ project/proj3_fly/src/Env.py
@@ -1,6 +1,8 @@
 import pybullet as p
 import os
 import Helper
+import random
+import math
 
 
 class Env(object):
@@ -13,15 +15,12 @@ class Env(object):
         robotPath = Helper.findURDF(robotName)
         self.initPos = initPos
         self.robotId = p.loadURDF(robotPath, initPos)
-
-        # p.changeDynamics(self.robotId, 0, linearDamping=0.0, jointDamping=0.0)
-        # p.changeDynamics(self.robotId, 1, linearDamping=0.0, jointDamping=0.0)
-        # p.changeDynamics(self.robotId, 2, angularDamping=0.0, jointDamping=0.0)
         
-        jointFrictionForce = 0.1
+        jointFrictionForce = 0.01
         for joint in range(p.getNumJoints(self.robotId)):
             p.setJointMotorControl2(self.robotId, joint, p.POSITION_CONTROL, force=jointFrictionForce)
-
+        
+        self.noise = 0.03
     
     def cameraControl(self):
         # control camera
@@ -31,6 +30,12 @@ class Env(object):
     def control(self, force):
         # apply force control
         # force: list of 2 float, input force signal for two engine
+        for motor in range(2):
+            if math.fabs(force[motor]) >= 100:
+                if force[motor] > 0:
+                    force[motor] = 100 * random.uniform(1 - self.noise, 1 + self.noise)
+                else:
+                    force[motor] = -100 * random.uniform(1 - self.noise, 1 + self.noise)
+            force[motor] *= random.uniform(1 - self.noise, 1 + self.noise)
         p.applyExternalForce(self.robotId, 3, [0, 0, force[0]], [0, 0, 0], p.LINK_FRAME)
         p.applyExternalForce(self.robotId, 4, [0, 0, force[1]], [0, 0, 0], p.LINK_FRAME)
-
