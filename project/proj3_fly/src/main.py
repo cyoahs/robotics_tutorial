aef913d88a5d74534b4acf13b6cbbe75a0521a3d
ycshao <shaoyecheng@zju.edu.cn>
2020-03-22 11:53:23 +0800

    record video

--- project/proj3_fly/src/main.py
+++ project/proj3_fly/src/main.py
@@ -1,8 +1,13 @@
 import pybullet as p
 import time
+import Helper
 
 from Env import Env
 import RobotControl
+
+# video flag
+recordVideo = True
+prefix = time.strftime('%Y%m%d%H%M%S',time.localtime(time.time()))
 
 p.connect(p.GUI)
 p.setGravity(0.0, 0.0, -10.0)
@@ -16,6 +21,10 @@ for jointId in range(p.getNumJoints(env.robotId)):
     print(p.getJointInfo(env.robotId, jointId))
     p.enableJointForceTorqueSensor(env.robotId, jointId, 1)
 
+if recordVideo:
+    videoFile = Helper.findLog(prefix+'.mp4')
+    videoLogId = p.startStateLogging(p.STATE_LOGGING_VIDEO_MP4, videoFile)
+
 while True:
     p.stepSimulation()
     time.sleep(1/240)
@@ -25,3 +34,6 @@ while True:
 
     env.cameraControl()
     RobotControl.addDebugItems()
+
+if recordVideo:
+    p.stopStateLogging(videoLogId)
