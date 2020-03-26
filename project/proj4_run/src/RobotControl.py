74e1454f10d1fe2028fa80db3d9e605d5ca4c54d
Yecheng Shao <shaoyecheng@zju.edu.cn>
2020-03-27 00:43:56 +0800

    Comment answers by ta

--- project/proj4_run/src/RobotControl.py
+++ project/proj4_run/src/RobotControl.py
@@ -1,6 +1,6 @@
 import pybullet as p
 import Helper
-import AnswerByTA
+# import AnswerByTA
 
 def loadRobot(initPos):
     robotName = 'MarioTheRobot.urdf'
@@ -11,14 +11,14 @@ def generateTraj(robotId):
     # work in this function to make a plan before actual control
     # the output can be in any data structure you like
     plan = None
-    plan = AnswerByTA.generateTraj(robotId)
+    # plan = AnswerByTA.generateTraj(robotId)
     return plan
 
 def realTimeControl(robotId, plan):
     # work in this function to calculate real time control signal
     # the output should be a list of two float
     controlSignal = [50, 50]
-    controlSignal = AnswerByTA.realTimeControl(robotId, plan)
+    # controlSignal = AnswerByTA.realTimeControl(robotId, plan)
     return controlSignal
 
 def addDebugItems(robotId):
